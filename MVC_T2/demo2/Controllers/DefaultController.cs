c3e28c65ec2280ab35a6fe12c820f1f82b6d42ea
WeiPei2219062260 <2219062260@qq.com>
2020-05-28 20:08:06 +0800

    添加了第二个作业小练习

---
+++ MVC_T2/demo2/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using demo2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            StudentEntities entities = new StudentEntities();
+            List<demo2.Models.Student_Data> studentList = entities.Student_Data.ToList();
+            ViewBag.studentList = studentList;
+            return View();
+        }
+    }
+}
