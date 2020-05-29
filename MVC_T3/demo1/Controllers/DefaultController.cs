63ac39ca9bfa70d7fdb67e02564fe6475d36f663
WeiPei2219062260 <2219062260@qq.com>
2020-05-29 16:10:04 +0800

    提交了T3的第一小节作业

---
+++ MVC_T3/demo1/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userEmail, string userPwd)
+        {
+            if (userEmail.Equals("123@qq.com") && userPwd.Equals("123"))
+            {
+                ViewBag.userEmail = userEmail;
+                ViewBag.userPwd = userPwd;
+                return View();
+            }
+            else
+            {
+                ViewBag.Msg = "输入有误！";
+                return View("Index");
+            }
+        }
+    }
+}
