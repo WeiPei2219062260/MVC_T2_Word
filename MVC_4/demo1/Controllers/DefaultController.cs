13d1243290896ef8d0de83a7a3f4d9985a3c56c2
WeiPei2219062260 <2219062260@qq.com>
2020-06-04 17:18:05 +0800

    T4_1

---
+++ MVC_4/demo1/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Show(int Id, string Name)
+        {
+            ViewBag.Id = Id;
+            ViewBag.Name = Name;
+
+            return View();
+        }
+    }
+}
---
+++ MVC_4/demo1/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register(int Id, string Name)
+        {
+            ViewBag.Id = Id;
+            ViewBag.Name = Name;
+            return View();
+        }
+    }
+}
